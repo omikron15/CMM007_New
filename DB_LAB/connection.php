39c896417a84d7ea8a2ec3873a97854a2d265061
1005104 <omikron15@hotmail.co.uk>
2016-04-12 11:08:37 +0100

    End of Basic Lab

---
+++ DB_LAB/connection.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net' );
+define('DB_USERNAME', 'b35575206e4fd8' );
+define('DB_PASSWORD','b55d73a3' );
+define('DB_DATABASE', 'Lab_DB');
+
+?>
---
+++ DB_LAB/login.php
@@ -0,0 +1,25 @@
+<?php
+include("connection.php");
+
+if (empty($_POST["username"]) || empty($_POST["password"]))
+{
+    echo "Please enter information for both fields";
+}
+else {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    $sql = "SELECT uid FROM users WHERE username= '$username' and password= '$password'";
+
+    $result = mysqli_query($db, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        header("location: home.php"); //Redirect to new page
+    } else {
+        echo "Invalid username/password combination";
+    }
+
+}
+
+?>
