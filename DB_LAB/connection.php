7c45d9bf4eadf95c3d3b2756dbbe92929bdf522a
1005104 <omikron15@hotmail.co.uk>
2016-04-14 09:38:54 +0100

    Lab attempt 2

--- DB_LAB/connection.php
+++ DB_LAB/connection.php
@@ -3,6 +3,8 @@
 define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net' );
 define('DB_USERNAME', 'b35575206e4fd8' );
 define('DB_PASSWORD','b55d73a3' );
-define('DB_DATABASE', 'Lab_DB');
+define('DB_DATABASE', 'acsm_3ff19214b36f21e');
+
+//acsm_3ff19214b36f21e;Data Source=eu-cdbr-azure-north-e.cloudapp.net;User Id=b35575206e4fd8;Password=b55d73a3
 
 ?>
