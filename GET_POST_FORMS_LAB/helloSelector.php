559388cad7bf20dacc13d74d2d3c64af2dc9d472
1005104 <omikron15@hotmail.co.uk>
2016-04-19 09:51:06 +0100

    Lab get post anf forms

--- GET_POST_FORMS_LAB/helloSelector.php
+++ GET_POST_FORMS_LAB/helloSelector.php
@@ -1,7 +1,19 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP Login Form without Session</title>
+</head>
+<body>
+<h1>Planets</h1>
+<h2>Where would you like to say hello? </h2>
 
-echo "Where would you like to say hello?";
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.php?planet=Earth">Earth</a>
 
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.phpplanet=Mars">Mars</a>
 
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.phpplanet=Uranus">Uranus</a>
 
-?>
+</body>
+</html>
+
