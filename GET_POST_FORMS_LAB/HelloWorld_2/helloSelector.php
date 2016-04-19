ba2263b8edc6e39ed7ab3a66fc426de5c5f02dfa
1005104 <omikron15@hotmail.co.uk>
2016-04-19 10:09:29 +0100

    new

---
+++ GET_POST_FORMS_LAB/HelloWorld_2/helloSelector.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP Login Form without Session</title>
+</head>
+<body>
+<h1>Planets</h1>
+<P>Where would you like to say hello? </P>
+
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/HelloWorld_2/helloprinter.php?planet=Earth">Earth</a> <br>
+
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/HelloWorld_2/helloprinter.php?planet=Mars">Mars</a> <br>
+
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/HelloWorld_2/helloprinter.php?planet=Uranus">Uranus</a>  <br>
+
+</body>
+</html>
+
