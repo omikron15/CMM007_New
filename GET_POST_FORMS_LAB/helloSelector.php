9e125b043c7d003edbde11cac0c35f1d5cbd80e7
1005104 <omikron15@hotmail.co.uk>
2016-04-19 09:53:54 +0100

    Lab get post anf forms

--- GET_POST_FORMS_LAB/helloSelector.php
+++ GET_POST_FORMS_LAB/helloSelector.php
@@ -8,11 +8,11 @@
 <h1>Planets</h1>
 <P>Where would you like to say hello? </P>
 
-<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.php?planet=Earth">Earth</a> ."<br/>"
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.php?planet=Earth">Earth</a> "<br/>"
 
-<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.phpplanet=Mars">Mars</a> ."<br/>"
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.phpplanet=Mars">Mars</a> "<br/>"
 
-<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.phpplanet=Uranus">Uranus</a> ."<br/>"
+<a href="http://cmm007.azurewebsites.net/GET_POST_FORMS_LAB/helloprinter.phpplanet=Uranus">Uranus</a> "<br/>"
 
 </body>
 </html>
