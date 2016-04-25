3f7972efc59d0c3efd2801f1c65fcd8d04038d69
1005104 <omikron15@hotmail.co.uk>
2016-04-25 09:56:40 +0100

    Cookie Login Page

---
+++ Cookies_LAB/Login.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Cookie Login Page</title>
+</head>
+<body>
+<h1>Cookie Login Page</h1>
+<div class="loginBox">
+    <h3>Login Form</h3>
+    <br><br>
+    <form>
+        <label>Username:</label><br>
+        <input type="text" name="username" placeholder="username"
+            /><br><br>
+        <label>Password:</label><br>
+        <input type="password" name="password" placeholder="password" />
+        <br><br>
+        <input type="submit" name="submit" value = "login"/>
+    </form>
+    <div class="error"><?php //echo $error;?><?php //echo $username; echo
+        $password;?></div>
+</div>
+</body>
+</html>
