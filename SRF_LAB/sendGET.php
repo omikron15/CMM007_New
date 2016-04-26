12c5cf87437f27218dd13c2ab6b35127a26ae8e9
1005104 <omikron15@hotmail.co.uk>
2016-04-26 12:34:37 +0100

    Session

---
+++ SRF_LAB/sendGET.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SendGET</title>
+</head>
+<body>
+<h1>SendGET</h1>
+
+<a href="http://cmm007.azurewebsites.net/SRF_LAB/printGET.php">LINK</a> <br>
+
+</body>
+</html>
+
